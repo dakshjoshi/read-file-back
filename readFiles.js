a2051d8b616b49e222aa25b1bedbd9a5eb1b2696
Daksh <jo.dakshjoshi@gmail.com>
2021-04-25 01:57:47 +0530

    initial comit

--- readFiles.js
+++ readFiles.js
@@ -7,30 +7,6 @@ const PORT = process.env.PORT || 6969;
 //MIDDLE WARE
 App.use(cors());
 
-App.get("/", function (req, res) {
-  res.json([
-    {
-      user: "Mr. Water Lyf",
-      forte: "Swimming",
-    },
-    {
-      user: "Mr. Walk Alot",
-      forte: "Walking",
-    },
-    {
-      user: "Mr. Runtster",
-      forte: "Running",
-    },
-    {
-      user: "Mr. Dee Znuts",
-      forte: "Football",
-    },
-    {
-      user: "Mr. No Stamina",
-      forte: "Jogging",
-    },
-  ]);
-});
 let directory = "SampleDir";
 // let files = fs.readdirSync(directory, { withFileTypes: true });
 // console.log(files);
@@ -38,7 +14,7 @@ let directory = "SampleDir";
 //Buffer.from(directory);
 
 //QUESTION : What is buffer ?
-App.get("/folder", (req, res) => {
+App.get("/", (req, res) => {
   fs.readdir(directory, { withFileTypes: true }, (err, files) => {
     if (err) {
       console.log(err);
